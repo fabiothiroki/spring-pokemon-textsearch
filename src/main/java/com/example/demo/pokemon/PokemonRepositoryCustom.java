11566c433bffc9b60f5dd0129e3878b3033e6782
Fabio Hiroki <fabio.hiroki@quintoandar.com.br>
2020-07-04 12:44:11 -0300

    It works

--- src/main/java/com/example/demo/pokemon/PokemonRepositoryCustom.java
+++ src/main/java/com/example/demo/pokemon/PokemonRepositoryCustom.java
@@ -1,13 +1,10 @@
 package com.example.demo.pokemon;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PokemonRepository extends CrudRepository<Pokemon, Long>, PokemonRepositoryCustom {
-
+public interface PokemonRepositoryCustom {
     List<Pokemon> search(String description);
-
 }
