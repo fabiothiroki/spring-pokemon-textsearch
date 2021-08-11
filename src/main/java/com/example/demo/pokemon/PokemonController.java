0786817615ef018427788a76739fa68f3054ae30
Fabio Hiroki <fabiothiroki@gmail.com>
2021-08-11 12:18:48 -0300

    add pagination example using spring data jpa

--- src/main/java/com/example/demo/pokemon/PokemonController.java
+++ src/main/java/com/example/demo/pokemon/PokemonController.java
@@ -3,12 +3,9 @@ package com.example.demo.pokemon;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.InputStream;
 import java.util.List;
@@ -34,6 +31,11 @@ public class PokemonController {
         return repository.search(search);
     }
 
+    @CrossOrigin
+    @GetMapping(value = "/all")
+    public List<Pokemon> findAll(@PageableDefault() Pageable pageable) {
+        return repository.findAll(pageable).getContent();
+    }
 
     @RequestMapping("/migrate")
     String migrate() {
--- src/main/java/com/example/demo/pokemon/PokemonRepository.java
+++ src/main/java/com/example/demo/pokemon/PokemonRepository.java
@@ -1,14 +1,15 @@
 package com.example.demo.pokemon;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PokemonRepository extends CrudRepository<Pokemon, Long> {
+public interface PokemonRepository extends PagingAndSortingRepository<Pokemon, Long> {
 
     @Query("SELECT p FROM Pokemon p WHERE fts(:description) = true")
     List<Pokemon> search(@Param("description") String description);
